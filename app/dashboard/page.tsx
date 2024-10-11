062f97fa8366cf8a8db8c62834bb6d903008424b
mrsajadpp <mrsajadpp@gmail.com>
2024-10-11 10:35:32 +0530

    bug fixed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,8 +6,7 @@ import DeskHeader from "@/components/dashboard_desktop_header";
 import Gride from "@/components/trending_gride";
 import Footer from "@/components/footer";
 import Link from "next/link";
-import "@/app/css/article.css";
-import "@/app/css/profile.css";
+import "@/app/css/dashboard.css";
 
 export const metadata = {
     title: "Trending Articles / Grovix Lab",
@@ -27,31 +26,6 @@ export default async function App() {
                 <div className="desktopMenu"></div>
                 <DeskHeader page={"dashboard"} path={path} />
                 <div className="mainContainer">
-                    <div className="fitCo">
-                        <div className="profile-container">
-                            <div className="profile-area">
-                                <div className="profile-image">
-                                    <div className="image">
-                                        <img src="https://lh3.googleusercontent.com/a/ACg8ocIrvLe0CF0XVFjtd7cC2STP1QZdcAWLzoZqK8W_bvEOhnQssmlh=s96-c" alt="" />
-                                    </div>
-                                    {/* <div className="follow-button">
-                                        <button>
-                                            <div className="icon">
-                                                <i className="fi fi-rr-pencil"></i>
-                                            </div>
-                                        </button>
-                                    </div> */}
-                                </div>
-                                <div className="profile-name">
-                                    <h2>Sajad PP</h2>
-                                    <span>Backend Web Developer, Founder of Grovix Lab.</span>
-                                </div>
-                            </div>
-                            <div className="metrices">
-                                <span>Other options are coming soon.</span>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
             <Footer />
